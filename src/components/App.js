3c9852d5bdb719aa78717272e406b20c93bf1fb7
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-09-12 18:55:07 +0000

    code done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[input,setInput] = useState('');
+  const [todos,setTodos] = useState([]);
+
+  function addTodo(){
+    if(input.trim()){
+      setTodos([...todos,input]);
+      setInput('');
+    }
+  };
+
+  function deleteTodos(index){
+   setTodos(todos.filter((todo,i)=> i!== index))
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1 style={{textAlign:"center"}}>To-to List</h1>
+       <input 
+       type="text" 
+       value={input}
+       onChange={(e)=>setInput(e.target.value)}>
+       </input>
+       <button onClick={addTodo}>Add Todo</button>
+       <ul>
+        {
+          todos.map((task,index)=>(
+            <li key={index}>
+               {task}
+               <button onClick={deleteTodos(index)}>Delete</button>
+            </li>
+          ))
+        }
+       </ul>
     </div>
   )
 }
